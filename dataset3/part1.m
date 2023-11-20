02530acb7fdec499f5cccd147a55d5a902fb76c4
Lucy <lululucyyyyyyy@gmail.com>
2023-11-20 15:26:53 -0500

    removed unnecessary files

--- dataset3/part1.m
+++
@@ -1,7 +0,0 @@
-fs = 11025;
-nBits = 16;
-recobj = audiorecorder(fs, nBits, 1);
-recordblocking(recobj, 1);
-word = getaudiodata(recobj);
-melSpectrogram(word, fs, 'NumBands', 16);
-S = melSpectrogram(word, fs, 'NumBands', 16);
--- dataset3/part2.m
+++
@@ -1,3 +0,0 @@
-filename = "bird1-220";
-writematrix(S, "spectrograms/b/" + filename + ".txt");
-audiowrite("wavs/b/" + filename + ".wav", word, fs);
